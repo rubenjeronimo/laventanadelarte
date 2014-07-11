2caac899b73daa78c2383a1b03e15963be3a189b
rubenjeronimo <ruben.jeronimo.fernandez@gmail.com>
2014-07-11 10:31:29 +0200

    Initial Commit

---
+++ LaVentanaDelArte/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LaVentanaDelArte
+//
+//  Created by Ruben Jeronimo Fernandez on 11/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LaVentanaDelArte/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  LaVentanaDelArte
+//
+//  Created by Ruben Jeronimo Fernandez on 11/07/14.
+//  Copyright (c) 2014 IronHack. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
